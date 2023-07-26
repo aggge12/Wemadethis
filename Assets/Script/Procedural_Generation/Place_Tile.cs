74c10baf499c6c2d23e004da8800a605155cb153
StonesRocks <68532873+StonesRocks@users.noreply.github.com>
2023-07-26 03:17:26 +0200

    Added some procedural generation
    made procedural generation for island maps.

--- Assets/Script/Procedural_Generation/Place_Tile.cs
+++ Assets/Script/Procedural_Generation/Place_Tile.cs
@@ -16,15 +16,17 @@ public class Place_Tile : MonoBehaviour
 
 
     [Header("Noise Textures")]
+    public Texture2D miniMap;
+    public float edge = 0.3f;
     public Texture2D foundation;
-    public float foundationRarity = 0.01f;
-    public float foundationSize = 0.02f;
+    public float foundationRarity = 0.015f;
+    public float foundationSize = 0.2f;
     public Texture2D biome;
     public float biomeRarity = 0.03f;
     public float biomeSize = 0.04f;
-    public Texture2D element;
-    public float elementRarity = 0.05f;
-    public float elementSize = 0.06f;
+    public Texture2D testMap;
+    public float testRarity = 0.05f;
+    public float testSize = 0.06f;
 
 
     void Start()
@@ -37,17 +39,23 @@ public class Place_Tile : MonoBehaviour
 
         seed = Random.Range(-10000, 10000);
 
+        miniMap = new Texture2D(worldSize, worldSize);
         foundation = new Texture2D(worldSize, worldSize);
         biome = new Texture2D(worldSize, worldSize);
-        element = new Texture2D(worldSize, worldSize);
+        testMap = new Texture2D(worldSize, worldSize);
 
         GenerateNoiseTexture(foundationRarity, foundationSize, foundation);
         GenerateNoiseTexture(biomeRarity, biomeSize, biome);
-        GenerateNoiseTexture(elementRarity, elementSize, element);
-        Debug.Log("Generated on Start");
+        GenerateNoiseTexture(testRarity, testSize, testMap);
+
+        MiniMapMaker();
+
     }
 
     // Update is called once per frame
+    private void Update() {
+        GenerateNoiseTexture(testRarity, testSize, testMap);
+    }
 
     public void GenerateNoiseTexture(float frequency, float limit, Texture2D noiseTexture) 
     {
@@ -56,15 +64,49 @@ public class Place_Tile : MonoBehaviour
             for (int y = 0; y < noiseTexture.height; y++)
             {
                 float v = Mathf.PerlinNoise((x + seed) * frequency, (y + seed) * frequency);
-                if (v > limit*1.1)
-                    noiseTexture.SetPixel(x, y, Color.green);
-                else
-                    noiseTexture.SetPixel(x, y, Color.black);
+                if (v > limit){
+                    noiseTexture.SetPixel(x, y, new Color(0,v,0));
+                }
+                else{
+                    noiseTexture.SetPixel(x,y, Color.black);
+                }
+
+
             }
         }
 
         noiseTexture.Apply();
     }
+    public void MiniMapMaker()
+    {
+        float p1;
+        float p2;
+        float px;
+        for (int y = 0; y <= worldSize/2; y++)
+        {
+            for (int x = 0; x <= worldSize; x++)
+            {
+                p1 = foundation.GetPixel(x, y).g;
+                p2 = foundation.GetPixel(worldSize-x, worldSize-y).g;
+                if (Mathf.Sqrt(Mathf.Pow(x-worldSize/2, 2) + Mathf.Pow(y-worldSize/2, 2)) > worldSize/2 * (1 - edge)){
+                    px = (p1+p2)/2 * (1-(Mathf.Sqrt(Mathf.Pow(x-worldSize/2, 2)+Mathf.Pow(y-worldSize/2, 2)) - worldSize/2*(1-edge))/(worldSize/2 * edge));
+                } else {
+                    px = (p1+p2)/2;
+                }
+                if (px > 0.5f){
+                    miniMap.SetPixel(x, y, new Color(0f, px, 0f));
+                    miniMap.SetPixel(worldSize-x, worldSize-y, new Color(0f, px, 0f));
+                } else {
+                    miniMap.SetPixel(x, y, new Color(0f, 0f, 0f));
+                    miniMap.SetPixel(worldSize-x, worldSize-y, new Color(0f, 0f, 0f));
+                }
+
+            }
+        }
+        miniMap.Apply();
+    }
+
+
     public void PlaceTile(TileBase tileSprite, int x, int y)
     {
         Tilemap tilemap = GetComponent<Tilemap>();
